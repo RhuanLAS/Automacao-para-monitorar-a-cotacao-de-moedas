5a2e21e85ca116ec9c1939804e295bea4b1d838d
Antonio R. Neto <antoniorolaneto216@gmail.com>
2020-09-03 23:15:41 -0300

    corrigindoBug1.2

--- hora.py
+++ hora.py
@@ -1,14 +1,16 @@
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
 
 def definindo_hora_inicial(driver):
-    if WebDriverWait(driver, 250).until(EC.title_contains("Brasil:")):
+    try:
+        WebDriverWait(driver, 250).until(EC.title_contains("Brasil:"))
         hora = driver.title
         hora = (hora.lstrip("Brasil: ")).replace(":", ".")
         #return float(hora)
         return hora
-    else:
-        assert False
+    except NoSuchElementException as e:
+        print(e)
 
 def hora_momento(driver_hora):
     hora_momento = definindo_hora_inicial(driver_hora)
--- links.py
+++ links.py
@@ -1,11 +1,15 @@
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 
 def precessa_moeda(driver, moeda, hora):
     xpath = moeda.mostra_xpath()
-    if WebDriverWait(driver, 250).until(EC.presence_of_element_located((By.XPATH, xpath))):
-        valor_str = driver.find_element_by_xpath(xpath).text
+    try:
+        WebDriverWait(driver, 250).until(EC.presence_of_element_located((By.XPATH, xpath)))
+        valor_str = driver.find_element_by_xpath(xpath)
+        valor_str = valor_str.get_attribute('textContent')
+        print(valor_str)
         moeda.cria_valor_momento(valor_str, hora)
-    else:
-        assert False
+    except NoSuchElementException as e:
+        print(e)
