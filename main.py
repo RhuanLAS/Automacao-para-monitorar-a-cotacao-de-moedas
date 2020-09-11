a138a2572444596974d27fb45a2d0d76f48b1ea8
Antonio R. Neto <antoniorolaneto216@gmail.com>
2020-09-11 19:21:10 -0300

    iniciandoNovamente

--- main.py
+++ main.py
@@ -1,63 +1,17 @@
-from selenium import webdriver
-from tipomoeda import Moeda
-import time
-import links
-import hora
+from urllib import request
+import json
 import menu
 
-######### Chamando o menu 1 para saber o driver do Chrome #########
+escolha = menu.menu_bem_vindo()
+if escolha == 1:
+    moedas_escolhidas = menu.menu_da_op_1()
 
-localizacao_driver_Chrome = menu.menu1()
+else:
+    pass
 
-######### Chamando o menu para saber o intervalo de tempo #########
 
-intevalo_de_tempo, quant_vezes_no_tempo = menu.menu()
-
-############## Achando a hora para inicio do programa #############
-
-driver_hora = webdriver.Chrome(localizacao_driver_Chrome) # C:\Program Files (x86)\chromedriver.exe
-driver_hora.get("https://time.is/pt_br/Brazil")
-hora_momento = hora.hora_momento(driver_hora)
-
-################# Vendo valores para as moedas ####################
-
-peso_argentino = Moeda('Peso Argentino','/html/body/div[4]/div/div[1]/div[2]/div/div/table/tbody/tr[1]/td[3]', hora_momento)
-dolar_canadense = Moeda('Dólar Canadense','/html/body/div[4]/div/div[1]/div[2]/div/div/table/tbody/tr[3]/td[3]', hora_momento)
-dolar_americano = Moeda('Dólar Americano','/html/body/div[4]/div/div[1]/div[2]/div/div/table/tbody/tr[6]/td[3]', hora_momento)
-euro = Moeda('Euro','/html/body/div[4]/div/div[1]/div[2]/div/div/table/tbody/tr[7]/td[3]', hora_momento)
-libra = Moeda('Libra','/html/body/div[4]/div/div[1]/div[2]/div/div/table/tbody/tr[8]/td[3]', hora_momento)
-
-driver_moedas = webdriver.Chrome(localizacao_driver_Chrome) # C:\Program Files (x86)\chromedriver.exe
-driver_moedas.get("https://www.infomoney.com.br/ferramentas/cambio/")
-
-for i in range(0,quant_vezes_no_tempo): # quantas vezes vai rodar
-    links.precessa_moeda(driver_moedas, peso_argentino, hora_momento)
-    links.precessa_moeda(driver_moedas, dolar_canadense, hora_momento)
-    links.precessa_moeda(driver_moedas, dolar_americano, hora_momento)
-    links.precessa_moeda(driver_moedas, euro, hora_momento)
-    links.precessa_moeda(driver_moedas, libra, hora_momento)
-    if i != quant_vezes_no_tempo-1:
-        time.sleep(intevalo_de_tempo)
-        hora_momento = hora.hora_momento(driver_hora)
-        driver_moedas.refresh()
-
-driver_moedas.close()
-driver_hora.close()
-
-print(f" Peso Argentino: {peso_argentino}")
-print(f" Dólar Canadense: {dolar_canadense}")
-print(f" Dólar Americano: {dolar_americano}")
-print(f" Euro: {euro}")
-print(f" Libra: {libra}")
-
-peso_argentino.coloca_dados_no_arquivo()
-dolar_canadense.coloca_dados_no_arquivo()
-dolar_americano.coloca_dados_no_arquivo()
-euro.coloca_dados_no_arquivo()
-libra.coloca_dados_no_arquivo()
-
-########################### OBS ####################################
-# 1 - Pensar em um jeito do arquivo ficar mais organizado
-# 2 - Plotar um gráfico ao término da execução do programa
-# 3 - Ter a possibilidade de plotar um gráfico com as infos do arquivo
-# 4 - Apresentar ao usuário as moedas que ele deseja saber o valor. Ex: um menu para isso
+# r = request.urlopen("http://economia.awesomeapi.com.br/json/all")
+# data = json.load(r)
+# print(data)
+# print()
+# print(type(data))
--- menu.py
+++ menu.py
@@ -1,19 +1,74 @@
 
-def menu():
-    print(" Deseja obter os dados a cada quanto tempo? ")
-    escolha = int(input(" Por favor, nos informe o tempo em minutos: "))
-    escolha *= 60
-    print(" Além disso, quantas vezes quer que obtenha os dados? ")
-    escolha1 = int(input(" Quantidade: "))
-    return escolha, escolha1
-
-def menu1():
-    decoracao = "~" * 15
-    print(end="\n")
-    print(f" {decoracao} Bem Vindo {decoracao}")
-    print(end="\n")
-    print(" Para a utilização do programa precisamos ter o driver do navegador (Chrome).")
-    print(" Por favor, caso não tenha o driver baixado faça o download para continuar.")
-    print(" Localização do driver na sua máquina. Por exemplo, C:\Program Files (x86)\chromedriver.exe")
-    escolha = input(" Localização: ")
+def menu_bem_vindo():
+    decoracao = "*" * 30
+    print()
+    print(f" {decoracao} BEM VINDO {decoracao}")
+    print()
+    escolha = menu_inicial()
     return escolha
+
+def menu_inicial():
+    decoracao = "+" * 28
+    while True:
+        print(f" {decoracao} MENU INICIAL {decoracao}")
+        print()
+        print(" 1 - Para obter dados novos")
+        print(" 2 - Para acessar dados já armazenados")
+        print(" 0 - Sair")
+        escolha = int(input(" Sua escolha: "))
+        if escolha > 0 and escolha <= 2:
+            break
+        elif escolha == 0:
+            mensagem_saida_programa()
+            exit()
+        else:
+            print(f" Esta entrada: {escolha} é inválida. Digite novamente")
+            print()
+
+    return escolha
+
+def mensagem_saida_programa():
+    print()
+    print(" Obrigado por usar o programa !!")
+
+def menu_da_op_1():
+    moedas_usuario = []
+    print()
+    print(" Aqui está todas as moedas que oferecemos para obtenção de informações: ")
+    print(" Dólar Comercial              Dólar Turismo              Dólar Canadense")
+    print(" Dólar Australiano            Euro                       Libra Esterlina")
+    print(" Peso Argentino               Iene Japonês               Franco Suíço")
+    print(" Yuan Chinês                  Novo Shekel Israelense     Bitcoin")
+    print(" Litecoin                     Ethereum                   Ripple")
+    print()
+    print(" Precisamos saber qual(ais) moeda(s) você deseja obter os dados.")
+    print(" Obs: certifique-se que a entrada seja exatamente o que está nas informções acima.")
+    while True:
+        moeda_escolha = input(" Escolha: ")
+        moeda_escolha = moeda_escolha.strip()
+        if verifica(moeda_escolha, moedas_usuario):
+            moedas_usuario.append(moeda_escolha)
+            decisao_usuario = decisao()
+            if decisao_usuario == 'N':
+                break
+        else:
+            print(" ERRO: Você pode ter digitado uma moeda novamente ou ela não está presente em nossa lista.")
+            print()
+    return moedas_usuario
+
+def verifica(moeda, moedas_usuario): # melhorar a verificação das moedas depois
+    lista_moedas_validas = ["DólarComercial", "DólarTurismo", "DólarCanadense", "DólarAustraliano", "Euro", "LibraEsterlina", "PesoArgentino", "IeneJaponês", "FrancoSuíço", "YuanChinês", "NovoShekelIsraelense", "Bitcoin", "Litecoin", "Ethereum", "Ripple"]
+    if moeda in lista_moedas_validas and moeda not in moedas_usuario:
+        return True
+    else:
+        return False
+
+def decisao():
+    print()
+    decisao = (input(" Deseja continuar escolhendo alguma moeda? [S] - SIM e [N] - NÃO: ")).upper()
+    if decisao != "S" and decisao != "N":
+        print(" Você digitou uma letra não permitida. Digite novamente.")
+        print()
+        decisao()
+
+    return decisao
